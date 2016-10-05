9501eb7fa4cc6fad4506ed87a91ce7260b0b7428
VitaoCeub <asgvitor@gmail.com>
2016-10-05 14:47:11 -0300

    Add files via upload

--- AppMain.java
+++ AppMain.java
@@ -1,3 +1,9 @@
+package analizador;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,11 +16,21 @@
  */
 public class AppMain {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        System.out.println("Digite a cadeia de caracteres na Linguagem Java a serem lidos:");
+        System.out.println("Digite FIM ou fim para parar a leitura!");
+        String programa = "";
+        Scanner leia = new Scanner (System.in);
+        boolean loop = true;
+        while (loop){
+            String lido = leia.nextLine();
+            if (lido.equals("FIM")|| lido.equals("fim")) {
+                loop = false;
+            } else{
+                programa+=lido+" ";
+            }
+        } 
+        AnalisadorLexico alexico = new AnalisadorLexico(programa);
+        alexico.analisar();
     }
-    
 }
---
+++ GerarNovoLexico.java
@@ -0,0 +1,29 @@
+package analizador;
+
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vitor_000
+ */
+public class GerarNovoLexico {
+    // Classe usada apenas para gerar uma nova classe Lexer, não sendo mais necessária.
+    /*
+    public static void main(String[] args) {
+        String path = "C:\\Users\\vitor_000\\Documents\\NetBeansProjects\\AnalizadorLexicoVG\\src\\Lexer.flex";
+        generarLexer(path);
+        
+    }
+    public static void generarLexer(String path){
+        File file=new File(path);
+        jflex.Main.generate(file);
+    }
+    */
+}
